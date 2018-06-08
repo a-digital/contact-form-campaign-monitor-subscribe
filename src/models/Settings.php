2f40a5e05d438ba377651d6d6dc6ace599a9d738
Mark Bowman <mark@adigital.co.uk>
2018-06-08 12:25:01 +0100

    Add attribute to settings model

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -39,7 +39,7 @@ class Settings extends Model
      *
      * @var string
      */
-    public $someAttribute = 'Some Default';
+    public $optInInputName = 'mailing-list';
 
     // Public Methods
     // =========================================================================
